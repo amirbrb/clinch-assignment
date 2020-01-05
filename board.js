b90e1ff9fe9fbfb6c700c9ba210fbf00c9d6ca38
Amir mishori <amirbrb@gmail.com>
2020-01-05 13:10:39 +0200

    comments

--- board.js
+++ board.js
@@ -1,6 +1,7 @@
 function Board () {
     cells = {};
-    this.loopers = {}
+    //this property holds all cells which are looping over each other (e.g - a2 and a3 holds a1 in its formula, this object will hold {"a1": ["a2", "a3"]})
+    this.loopers = {} 
 
     this.addCell = function (cell){
         cells[cell.key] = cell;
