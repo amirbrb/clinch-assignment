03e3e211aa6ac964c9be874736230ce49f09eb01
Amir mishori <amirbrb@gmail.com>
2020-01-05 13:13:42 +0200

    Comments

--- cell.js
+++ cell.js
@@ -5,21 +5,27 @@ function Cell(key, parentBoard) {
     this.isValid = true; 
     this.parentBoard = parentBoard;
 
+    //sets cell's value
     this.set = function (userInput) {
         this.userInput = userInput.toString();
         let res = calculate (userInput, this.parentBoard, this.key);
         this.result = res.result;
         this.isValid = res.isValid;
 
-        let board = this.parentBoard 
+        let board = this.parentBoard
+        
+        //in case this cell is referenced by others - update other cells value
         if(board.loopers[this.key]) {
             board.loopers[this.key].forEach(cellKey => {
                 let cell = board.getCell(cellKey); 
+                
+                //set referencing cell's value
                 cell.set(cell.userInput);
             })
         }
     }
 
+    //get cell's value (1 is formula, 2 is calculated result 
     this.get = function (mode) {
         if(mode == 1) {
             return this.userInput;
@@ -30,6 +36,8 @@ function Cell(key, parentBoard) {
 
     function calculate(userInput, parentBoard, key) {
         const input = userInput.toString(); 
+        
+        //not a formula
         if(!input.startsWith('=')) {
             return  {
                 isValid: true,
@@ -38,6 +46,8 @@ function Cell(key, parentBoard) {
         }
         else {
             let formula = input.substr(input.indexOf('=') + 1)
+            
+            //sum formula
             if(formula.toLowerCase().startsWith('sum')) {
                 return calculateSum(formula);
             } else {
@@ -74,6 +84,7 @@ function Cell(key, parentBoard) {
         }
     }
 
+    //calculates all numbers with + sign
     function getMathematicResult(input, formula, parentBoard, key) {
         let formulaValues = formula.split('+');
         let result = 0;
